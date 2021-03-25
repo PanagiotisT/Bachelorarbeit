160bd9345c8a0083ad84b63ab2b6e43d466cae86
Tsitsos <Panagiotis.tsitsos@adesso.de>
2021-03-26 00:22:41 +0100

    Angular - scene overview component

--- reading-fluency-application/src/app/scene-overview/scene-overview.component.ts
+++ reading-fluency-application/src/app/scene-overview/scene-overview.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { IStory } from '../entities/i-story';
+import { LogService } from '../services/log.service';
+import { UnityWebGlComponent } from '../unity-web-gl/unity-web-gl.component';
 
 @Component({
   selector: 'app-scene-overview',
   templateUrl: './scene-overview.component.html',
   styleUrls: ['./scene-overview.component.scss']
 })
-export class SceneOverviewComponent implements OnInit {
+export class SceneOverviewComponent implements OnInit{
 
-  constructor() { }
+  @ViewChild(UnityWebGlComponent) child: UnityWebGlComponent;
+
+  story: IStory;
+  sceneIndex: number;
+
+  messageFromUnity = 'Hier erscheint die Nachricht die von Unity empfangen wird.';
+
+  constructor(private dataService: DataService, private logService: LogService) {
+    this.dataService.currentStory.subscribe( story => {
+      this.story = story;
+      console.log(this.constructor.name);
+      logService.object(this, story);
+    });
+
+    this.dataService.currentIndex.subscribe( index => {
+      this.sceneIndex = index;
+      console.log(this.constructor.name + '  ' + index);
+    });
+
+    logService.text(this, 'Constructor message!');
+  }
 
   ngOnInit(): void {
+    (window as any).sendButtonMessageListener = (message: string) => {
+      this.messageFromUnity = message;
+    };
+  }
+
+  selectScene(index: number) {
+    console.log(this.constructor.name);
+    this.dataService.setScene(index);
+    this.child.changeUnityScene();
   }
 
 }
