4f5aac63e99fe42b04e58cd84cb9e97a045b704d
Tsitsos <Panagiotis.tsitsos@adesso.de>
2021-03-25 16:54:07 +0100

    Angular - created services

--- reading-fluency-application/src/app/app.module.ts
+++ reading-fluency-application/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiService } from './services/api.service';
+import { DataService } from './services/data.service';
+import { LogService } from './services/log.service';
+import { SocketService } from './services/socket.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ApiService, DataService, LogService, SocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
