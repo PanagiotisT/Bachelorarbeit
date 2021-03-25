348021f866a75fd97b249f7a8665112a593a4675
Tsitsos <Panagiotis.tsitsos@adesso.de>
2021-03-25 22:26:58 +0100

    Angular - log service

--- reading-fluency-application/src/app/services/log.service.ts
+++ reading-fluency-application/src/app/services/log.service.ts
@@ -5,5 +5,19 @@ import { Injectable } from '@angular/core';
 })
 export class LogService {
 
+  titleStyle = 'font-size: 16px; font-weight: bold';
+  textStyle = 'font-size: 13px;';
   constructor() { }
+
+  object(clazz, objectToLog) {
+    console.log(`${this.logTime(clazz)} %o`, this.titleStyle, objectToLog);
+  }
+
+  text(clazz, msg) {
+    console.log(`${this.logTime(clazz)} %c${msg}` , this.titleStyle, this.textStyle);
+  }
+
+  logTime(clazz): string {
+    return `%c ${new Date().toLocaleString()} - ${clazz.constructor.name}`;
+  }
 }
