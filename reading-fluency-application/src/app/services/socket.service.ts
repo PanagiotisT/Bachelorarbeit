2fd3b2f1f17c2121e822c881da70ecac485dcd43
Tsitsos <Panagiotis.tsitsos@adesso.de>
2021-03-25 22:27:35 +0100

    Angular - socket service

--- reading-fluency-application/src/app/services/socket.service.ts
+++ reading-fluency-application/src/app/services/socket.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import * as io from 'socket.io-client';
+import { Observable } from 'rxjs';
+import { LogService } from './log.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SocketService {
+export class SocketService implements OnDestroy {
 
-  constructor() { }
+  socket: any;
+  readonly uri: string = 'http://localhost:3000';
+
+  constructor(private logService: LogService) {
+    // @ts-ignore
+    this.socket = io(this.uri);
+    console.log('Socket Service Constructor');
+  }
+
+  listen(eventName: string) {
+    return new Observable( (subscriber => {
+      this.socket.on(eventName, (data) => {
+        subscriber.next(data);
+      });
+    }));
+  }
+
+  emit(eventName, data) {
+    this.socket.emit(eventName, data);
+  }
+
+  ngOnDestroy() {
+    this.logService.text(this, 'On Destroy');
+  }
+
 }
