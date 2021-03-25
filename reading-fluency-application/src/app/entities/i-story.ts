e6ec8740097af21b8b1446519bccc4e3faec5184
Tsitsos <Panagiotis.tsitsos@adesso.de>
2021-03-25 16:54:46 +0100

    Angular - create interfaces for mongoDb data

---
+++ reading-fluency-application/src/app/entities/i-story.ts
@@ -0,0 +1,20 @@
+export interface IStory {
+  name: string;
+  description: string;
+  image: string;
+
+  scenes: Array<IScene>;
+  characters: ICharacter;
+}
+
+export interface IScene {
+  name: string;
+  text: string;
+  image: string;
+}
+
+export interface ICharacter {
+  total: number;
+  teller: boolean;
+  character: Array<string>;
+}
