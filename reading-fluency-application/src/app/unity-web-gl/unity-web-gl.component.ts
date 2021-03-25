cb60c71f055227e8c973ff6247aa02ebd3dd705d
Tsitsos <Panagiotis.tsitsos@adesso.de>
2021-03-26 00:18:45 +0100

    Angular - import unity WebGL and add communication

--- reading-fluency-application/src/app/unity-web-gl/unity-web-gl.component.ts
+++ reading-fluency-application/src/app/unity-web-gl/unity-web-gl.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-unity-web-gl',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnityWebGlComponent implements OnInit {
 
-  constructor() { }
+  gameInstance: any;
+  progress = 0;
+  isReady = false;
+
+  currentIndex: number;
+
+  constructor(private dataService: DataService) {
+    this.dataService.currentIndex.subscribe( index => {
+      this.currentIndex = index;
+    });
+  }
 
   ngOnInit(): void {
+    const loader = (window as any).UnityLoader;
+
+    // Anpassen / Odnername wird immer der Story.name sein und der Index zeigt welche Szene geladen werden soll
+    this.gameInstance = loader.instantiate(
+        'gameContainer',
+        '/assets/Build/Unity.json', {
+          onProgress: (gameInstance: any, progress: number) => {
+            this.progress = progress;
+            if (progress === 1) {
+              this.isReady = true;
+            }
+          }
+        });
+  }
+
+  // Unity Funktion aufrufen:
+  // this.gameInstance.SendMessage(Parameter 1 = Das GameObject von Unity, welches angesprochen werden soll Bsp.: 'GameObject',
+  //                               Parameter 2 = Der Methodennamen aus dem GameObject Bsp.: 'ChangeDisplayText',
+  //                               Parameter 3 = Parameter für die aufgerufene Funktion die Übergeben werden sollen)
+
+
+  // Werte aus Unity empfangen:
+  // https://docs.unity3d.com/Manual/webgl-interactingwithbrowserscripting.html
+  // In Unity eine .jslib Datei erstellen Funktion erstellen, die einen Pointer aus einer C# Funktion bekommt und an eine window funktion sendet
+
+  changeUnityScene() {
+    this.gameInstance.SendMessage('SceneManager', 'loadSceneWithIndex', this.currentIndex);
   }
 
 }
